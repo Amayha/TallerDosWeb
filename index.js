e2de8fb7bcb83c8344ba5505b2f681e411a720ec
Pau <paula_ale_pe@hotmail.com>
2019-04-29 08:33:49 -0500

    Se conecta a mongo y lista de productos

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 
+
+var MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'peluditos';
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  
+    const db = client.db(dbName);
+
+    const productos = db.collection('productos');
+
+    productos.find({}, {sort: ['precio']}).toArray(function(err, docs){
+        assert.equal(null, err);
+        console.log('encontramos los docs');
+        docs.forEach(function(prod){
+            console.log(prod.precio);
+        })
+
+    });
+  
+    client.close();
+  });
+
+
+
 var app = express();
 
 app.use(express.static('public'));
